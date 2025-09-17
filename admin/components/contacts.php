4dbf2228885453e7743d3c4505fb59b95dca230a
Alisha Patel <patelalisha2166@gmail.com>
2025-09-17 19:19:08 +0530

    wedf

--- admin/components/contacts.php
+++ admin/components/contacts.php
@@ -1,70 +1,73 @@
- <div class="dashboard-container">
-     <div class="dashboard-header">
+<div class="dashboard-container">
+    <div class="dashboard-header">
 
-         <?php
-            echo "<h1>User Contacts</h1>";
-            echo '<div>Wellcome ' . $_COOKIE['admin'] . '</div>';
-            ?>
+        <?php
+        echo "<h1>User Contacts</h1>";
+        echo '<div>Welcome ' . $_COOKIE['admin'] . '</div>';
+        ?>
 
-     </div>
+    </div>
 
-     <div class="table-container">
-         <table id="contactsTable">
-             <thead>
-                 <tr>
-                     <th>Contact ID</th>
-                     <th>Full Name</th>
-                     <th>Email</th>
-                     <th>Subject</th>
-                     <th>Message</th>
-                     <th>Created At</th>
-                     <th>Actions</th>
-                 </tr>
-             </thead>
-             <tbody id="contactsBody">
+    <div class="table-container">
+        <table id="contactsTable">
+            <thead>
+                <tr>
+                    <th>Contact ID</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Subject</th>
+                    <th>Message</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody id="contactsBody">
                 <style>
                     .btn-edit-contact i,
-.btn-delete-contact i{
-    color: var(--white-color);
-}
+                    .btn-delete-contact i {
+                        color: var(--white-color);
+                    }
 
-.btn-edit-contact{
-    background-color: royalblue;
-    border: none;
-    border-radius: 0.3rem;
-    padding: 0.5rem 0.8rem;
-    cursor: pointer;
-    font-size: 1rem;
-    transition: background-color 0.3s;
-    margin-bottom: 0.3rem;
-}
-.btn-edit-contact:hover{
-    background-color: darkblue;
-}
-.btn-delete-contact{
-    background-color: crimson;
-    color: var(--white-color);
-    border: none;
-    border-radius: 0.3rem;
-    padding: 0.5rem 0.8rem;
-    cursor: pointer;
-    font-size: 1rem;
-    transition: background-color 0.3s;
-}
-.btn-delete-contact:hover{
-    background-color: darkred;
-}
+                    .btn-edit-contact {
+                        background-color: royalblue;
+                        border: none;
+                        border-radius: 0.3rem;
+                        padding: 0.5rem 0.8rem;
+                        cursor: pointer;
+                        font-size: 1rem;
+                        transition: background-color 0.3s;
+                        margin-bottom: 0.3rem;
+                    }
+
+                    .btn-edit-contact:hover {
+                        background-color: darkblue;
+                    }
+
+                    .btn-delete-contact {
+                        background-color: crimson;
+                        color: var(--white-color);
+                        border: none;
+                        border-radius: 0.3rem;
+                        padding: 0.5rem 0.8rem;
+                        cursor: pointer;
+                        font-size: 1rem;
+                        transition: background-color 0.3s;
+                    }
+
+                    .btn-delete-contact:hover {
+                        background-color: darkred;
+                    }
                 </style>
-                 <script>
-                     loader = document.getElementById("lineLoaderContainer") // Show loader
-                     axios.get('../api/get_contacts.php')
-                         .then(function(response) {
-                             const contacts = response.data;
-                             const contactsBody = document.getElementById('contactsBody');
+                <script>
+                    loader = document.getElementById("lineLoaderContainer") // Show loader
+                    axios.get('../api/get_contacts.php')
+                        .then(function (response) {
+                            const contacts = response.data;
+                            const contactsBody = document.getElementById('contactsBody');
 
-                             contacts.forEach(contact => {
-                                 const row = document.createElement('tr');
-                                 row.innerHTML = `
+                            contacts.forEach(contact => {
+                                const row = document.createElement('tr');
+                                row.innerHTML = `
                                         <td>${contact.id}</td>
                                         <td>${contact.full_name}</td>
                                         <td>${contact.email}</td>
@@ -76,18 +79,18 @@
                                             <button class="delete-button" onclick="deleteContact(${contact.id})"><i class="bi bi-trash"></i>Delete</button>
                                         </td>
                                     `;
-                                 contactsBody.appendChild(row);
-                                });
-                             
-                            })
-                            .catch(error => {
-                             console.error("Error fetching packages:", error);
-                             loader.innerHTML = `<p class="text-danger">❌ Failed to load packages.</p>`;
-                         }).finally(() => {
-                             document.getElementById("lineLoaderContainer").style.display = "none"; // Hide loader
-                         });
-                 </script>
-             </tbody>
-         </table>
-     </div>
- </div>
+                                contactsBody.appendChild(row);
+                            });
+
+                        })
+                        .catch(error => {
+                            console.error("Error fetching packages:", error);
+                            loader.innerHTML = `<p class="text-danger">❌ Failed to load packages.</p>`;
+                        }).finally(() => {
+                            document.getElementById("lineLoaderContainer").style.display = "none"; // Hide loader
+                        });
+                </script>
+            </tbody>
+        </table>
+    </div>
+</div>
